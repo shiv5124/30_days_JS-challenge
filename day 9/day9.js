c6e11dc8533750af781952c55c579b4b892be586
shiv5124 <shivamt5124@gmail.com>
2024-07-23 00:42:32 +0530

    day 9 finished

--- day 9/day9.js
+++ day 9/day9.js
@@ -8,7 +8,53 @@ document.getElementsByClassName("color")[0].style.backgroundColor = "red";
 
 //Task 3
 
-document.createElement("div");
+let newDiv = document.createElement("div");
 newDiv.id = "task3";
+newDiv.textContent = "30 days of JS"
+document.body.appendChild(newDiv)
 
-document.getElementById("task3").textContent = "30 days of JS"
+// Task 4
+
+const list = document.getElementById("list")
+const li = document.createElement("li")
+li.innerHTML = "JavaScript"
+list.appendChild(li)
+
+// Task 5
+
+const rem = document.getElementById("rem")
+rem.remove()
+
+// Task 6
+
+const second = document.getElementById("second-child")
+second.remove()
+
+// Task 7
+
+const func = () =>{
+    document.getElementById("myinp").setAttribute("type","button")
+}
+
+// Task 8
+
+document.getElementById("type1").classList.remove("red")
+document.getElementById("type2").classList.add("red")
+
+// Task 9
+
+const change = () =>{
+    document.getElementById("para1").textContent = "voila changed"
+}
+
+// Task 10 
+
+const border = document.getElementById("border")
+border.onmouseover = function(){
+    document.getElementById("border").classList.remove("redbd")
+    document.getElementById("border").classList.add("bluebd")
+}
+border.onmouseout = function(){
+    document.getElementById("border").classList.add("redbd")
+    document.getElementById("border").classList.remove("bluebd")
+}
