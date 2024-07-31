997da4885b5a703acc4263d75998d77d939b93cc
shiv5124 <shivamt5124@gmail.com>
2024-08-01 02:36:06 +0530

    3 tasks done of day 12

---
+++ day 12/day12.js
@@ -0,0 +1,37 @@
+// Task 1
+
+/*try{
+    throw new Error("some error occured")
+}catch(error){
+    console.log(error)
+}*/
+
+// Task 2
+
+/*const divison = (a,b) =>{
+    return a/b 
+}
+let a = 5;
+let b = 0;
+let c = divison(a,b)
+if(b === 0){
+    
+    try{
+        throw new Error("denominator cannot be zero")  
+    }catch(error){
+        console.log(error)
+    }
+}
+
+// Task 3
+
+try{
+throw new Error("some error came")
+}catch(error){
+console.log(error)
+}finally{
+console.log("finally block")
+}*/
+
+// Task 4
+
