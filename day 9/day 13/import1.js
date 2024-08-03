6a278884f6504301dc97bd64daea04e5d09c8476
shiv5124 <shivamt5124@gmail.com>
2024-08-04 02:11:35 +0530

    day 13 5 tasks

---
+++ day 9/day 13/import1.js
@@ -0,0 +1,54 @@
+// Task 1
+/*
+module.exports = {
+add: function(a,b){
+    return a + b
+} 
+}
+
+
+// Task 2
+
+export const person = {
+    name: 'jeff',
+    age: 25,
+    greet() {
+        return `Hello, my name is ${this.name}`;
+    }
+}
+
+
+// Task 3
+
+export function function1(a,b) {
+    console.log(a-b)
+}
+
+export function function2(x,y) {
+    console.log(x*y)
+}
+
+
+// Task 4
+
+export default function greet(name) {
+    console.log(`Hello, ${name}!`);
+}
+
+
+// Task 5
+
+export const obj1 = 'Hello'
+export const obj2 = 42
+
+export function func1(a,b) {
+    console.log(a+b)
+}
+
+export function func2(x,y) {
+    console.log(x-y)
+}
+*/
+
+
+
---
+++ day 9/day 13/main.js
@@ -0,0 +1,43 @@
+// Task 1
+/*
+const adding = require('./import1.js')
+
+const result = adding.add(5,10)
+console.log(result)
+
+
+// Task 2
+
+import { person } from './import1'
+
+console.log(person.greet())
+console.log(`Age: ${person.age}`)
+
+// Task 3
+
+import { function1, function2 } from './myModule.js'
+
+function1()
+function2()
+
+
+// Task 4
+
+import greet from './greet.js'
+
+greet('person1')
+greet('person2') 
+
+
+// Task 5
+
+// main.js
+import * as myModule from './myModule.js';
+
+console.log(myModule.obj1)
+console.log(myModule.obj2)
+
+myModule.func1()
+myModule.func2()
+*/
+
